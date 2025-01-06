450259b62af0153ec490f7bc72dcf709c72ed68f
gunny <ehdrjs515@gmail.com>
2025-01-06 14:43:51 +0900

    폴더 저장

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <form action="">
+    <div>
+      <label for="userId">ID:</label>
+      <input id="userId" typeof="text">
+    </div>
+    <div>
+      <label for="">PW:</label>
+      <input type="text">
+    </div>
+    <button type="submit">로그인</button>  
+  </form>
+</template>
+  
+<script>
+import axios from 'axios';
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style> 
